44c6615e5897cf244f56d9e3ca5dacfbae988e61
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2018-02-04 13:50:56 -0800

    Create readme.h

---
+++ src/Spark-fun/readme.h
@@ -0,0 +1,5 @@
+// JD's Office Light Level Readout Project
+
+// Displays the current temperature and light level in my office.
+// Also sends the data via serial bus so it can be seen on a computer
+// and eventually connected to the internet.
