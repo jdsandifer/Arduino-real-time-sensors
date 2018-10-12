b1e8e9a79619895583fcc42fc1973c2dd2a0efdf
J.D. Sandifer <sandifer.jd@gmail.com>
2018-10-11 18:14:28 -0700

    Reduce comments

--- src/Arduino-101/JDs_Office_Sensors.ino
+++ src/Arduino-101/JDs_Office_Sensors.ino
@@ -15,16 +15,15 @@ const int photoSensorPin = A0;
 const int temperaturePin = A1;
 
 int lightLevel = -1;
-int calibratedLightLevel = -2; // used to store the calibrated lightLevel (footcandles)
-int maxThreshold = 0;     // used for setting the "max" light level
-int minThreshold = 1023;   // used for setting the "min" light level
+int calibratedLightLevel = -2; // in footcandles
+int maxThreshold = 0;     // for light level
+int minThreshold = 1023;   // for light level
 
 // Define the B-value of the thermistor.
 // This value is a property of the thermistor used in the Grove - Temperature Sensor,
 // and used to convert from the analog value it measures and a temperature value.
 const int B = 3975;
 
-// Setup RGB backlight color on LCD
 const int colorR = 0;
 const int colorG = 96;
 const int colorB = 128;
@@ -33,39 +32,33 @@ void setup()
 {
   Serial.begin(9600);
 
-  // Initialize the 16x2 LCD, clear it, and say hi
   lcd.begin(16, 2); //Initialize the 16x2 LCD
   lcd.setRGB(colorR, colorG, colorB);
-  lcd.clear();  //Clear any old data displayed on the LCD
-  lcd.print("- JD's Office -"); // Display a message on the LCD!
+  lcd.clear();
+  lcd.print("- JD's Office -");
 }
 
 void loop()
 {
-  // Read and calibrate light level
-  lightLevel = analogRead(photoSensorPin);  // reads the voltage on the sensorPin
+  lightLevel = analogRead(photoSensorPin);
 
   // Conversion for using 5v power instead of 3.3v
   lightLevel = lightLevel / 1.515151;
   
   calibrateLightLevel(25);    // calibrates the light level with a calculated calibration ratio
   
-  // Log light levels (uncalibrated and calibrated) to serial port ("xxx,  xx fc")
   Serial.print("Light - Voltage: ");
   Serial.print(lightLevel);
   Serial.print(", Footcandles: ");
   Serial.print(calibratedLightLevel);
   Serial.print(";     ");
 
-  // Setup for temperature gathering
   float resistance;
   float degreesC;
   float degreesF;
 
-  // Get temperature from sensor
-  resistance = getResistance(temperaturePin); //Measure the voltage at the analog pin
+  resistance = getResistance(temperaturePin);
 
-  // Convert raw data to Fahrenheit and Celsius
   degreesC = 1/(log(resistance/10000)/B+1/298.15)-273.15;
 
   // Conversion for using 5v power instead of 3.3v
@@ -75,8 +68,6 @@ void loop()
     
   degreesF = degreesC * (9.0 / 5.0) + 32.0;
   
-  // Now print to the Serial monitor lines of data like this:
-  // "voltage: 0.73 deg C: 22.75 deg F: 72.96"
   Serial.print("Temperature - Resistance: ");
   Serial.print(resistance);
   Serial.print(", °C: ");
@@ -89,13 +80,12 @@ void loop()
   char degreesFChars[10];
   dtostrf(degreesF, 3, 1, degreesFChars);
 
-  // Display sensor info on the LCD
-  lcd.setCursor(0, 1);   // Start from to column 0, row 1 (second row)
+  lcd.setCursor(0, 1);   // Move to column 0, row 1 (second row)
   lcd.print(" ");
   lcd.print(degreesFChars);
   lcd.print((char)223);   // 223 represents '°' on this LCD (HD44780?)
   lcd.print("F  ");
-  if (calibratedLightLevel < 10 && calibratedLightLevel >= 0) lcd.print("0");   // add a zero for single digits
+  if (calibratedLightLevel < 10 && calibratedLightLevel >= 0) lcd.print("0");
   lcd.print(calibratedLightLevel);
   lcd.print(" fc"); 
   
@@ -109,11 +99,7 @@ void calibrateLightLevel(int calibrationConstant)
 
 float getResistance(int pin)
 {
-  // Get the raw value of the temperature sensor.
   int rawValue = analogRead(pin);
-  
-  /// Determine the current resistance of the thermistor based on the sensor value.
   float resistance = (float)(1023-rawValue)*10000/rawValue;
-
   return resistance;
 }
